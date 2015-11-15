97ca7b7c9234d70985886f044ec4811c07b0b4f5
Eli Weinstock-Herman <tarwn@tiernok.com>
2015-11-14 22:30:40 -0500

    Add main w/ github baseUrl

---
+++ Javascript/scripts/github_main.js
@@ -0,0 +1,9 @@
+﻿requirejs.config({
+    baseUrl: "http://tarwn.github.io/DistributedSamples/Javascript/scripts",
+    paths: {
+        "jquery":			"lib/jquery-1.11.3.min",
+        "knockout-base":    "lib/knockout-3.3.0",
+		"knockout":			"ext/knockout",
+        "bluebird":			"lib/bluebird.min"
+    }
+});
